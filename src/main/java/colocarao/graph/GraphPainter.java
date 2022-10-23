68eb53438242b8597655791b0fd85c8d7f7f099f
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-10-23 13:20:01 -0300

    Optimize Grasp Solution

--- src/main/java/colocarao/graph/GraphPainter.java
+++ src/main/java/colocarao/graph/GraphPainter.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 public class GraphPainter {
@@ -17,7 +18,7 @@ public class GraphPainter {
         this.numberOfIterations = numberOfIterations;
     }
 
-    public <T> Map<T, Integer>  paintGraph(Graph<T> graph) {
+    public <T> Map<T, Integer> paintGraph(Graph<T> graph) {
         Map<T, Integer> bestSolution = null;
 
         for (int i = 0; i < numberOfIterations; i++) {
@@ -32,7 +33,14 @@ public class GraphPainter {
         return bestSolution;
     }
 
-    private <T> Map<T, Integer>  buildInitialSolution(Graph<T> graph) {
+    /* Estrátegia para solução inicial
+    *  1. Percorrer a lista de ajacência
+    *  2. Para cada vértice, gerar um cor (número) randomico
+    *  3. Para cada cor gerada, verficar se algum vizinho não já está usando ela
+    *  4. Atribuir a cor gerado ao vértice
+    *  5. Próxima iteração
+    */
+    private <T> Map<T, Integer> buildInitialSolution(Graph<T> graph) {
         Map<T, Integer> solution = new HashMap<>();
 
         Map<T, List<T>> adjacencyList = graph.getAdjacencyList();
@@ -46,9 +54,36 @@ public class GraphPainter {
                 }
             }
 
-            int max = 0;
+            int color;
+            do {
+                color = new Random().nextInt(graph.getVertexCount());
+            } while (neighboorsColors.contains(color));
 
-            for (Integer color : neighboorsColors) {
+            solution.put(v, color);
+        }
+
+        return solution;
+    }
+
+    /* Estrátegia para Busca Local inicial
+     *  1. Percorrer a lista de ajacência
+     *  2. Para cada vértice, obter as cores dos vizinhos
+     *  3. Obter a maior cor utilizada pelos vizinhos
+     *  4. Percorrer de 0 até maior cor buscando a menor cor disponível (não utilizada pelos vizinhos).
+     *     Assim, busca-se reduzir o número de cores utilizadas, pois na fase de construção podem ser gerados valores
+     *     muito grandes.
+     */
+    private <T> void applyLocalSearch(Graph<T> graph, Map<T, Integer> solution) {
+        Map<T, List<T>> adjacencyList = graph.getAdjacencyList();
+
+        for (T v : adjacencyList.keySet()) {
+            Set<Integer> neighboorsColors = new HashSet<>();
+
+            int max = 0;
+            for (T e : adjacencyList.get(v)) {
+                int color = solution.get(e);
+                neighboorsColors.add(color);
+
                 if (color > max) {
                     max = color;
                 }
@@ -62,12 +97,6 @@ public class GraphPainter {
             }
 
         }
-
-        return solution;
-    }
-
-    private <T> void applyLocalSearch(Graph<T> graph, Map<T, Integer> solution) {
-
     }
 
     private <T> boolean hasImprovedSolution(Map<T, Integer> currentBestSolution, Map<T, Integer> newSolution) {
@@ -80,5 +109,4 @@ public class GraphPainter {
 
         return newTotalColors > currentTotalColors;
     }
-
 }
